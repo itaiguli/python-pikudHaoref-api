73931bf168a25c80d9feee2baa8667d7960fb1be
Itai Guli <click.itai@gmail.com>
2020-07-08 20:58:04 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'tzevaadom',
+  packages = ['tzevaadom'],
+  version = '1.0',
+  license='MIT',
+  description = 'A Python library for Pikud Haoref's unofficial rocket alert API.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Itai Guli',
+  author_email = 'app.redalert@gmail.com',
+  url = 'https://github.com/itaiguli',
+  download_url = 'https://github.com/itaiguli/guli/archive/v_01.tar.gz',
+  keywords = ['tzevaadom', 'pikud', 'oref', 'redalert', 'rocket'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
